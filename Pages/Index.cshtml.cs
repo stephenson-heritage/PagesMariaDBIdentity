46e5ed92f1c26ced184be8a21f932afce751c6b0
stephenson-heritage <LStephenson@cegep-heritage.qc.ca>
2021-03-29 12:39:42 -0400

    custom login and message

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
@@ -14,17 +15,26 @@ namespace PagesMariaDB.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly StoreContext _context;
+        private readonly UserManager<WebUser> _userManager;
 
-        public IndexModel(ILogger<IndexModel> logger, StoreContext context)
+
+        public IndexModel(ILogger<IndexModel> logger, StoreContext context, UserManager<WebUser> userManager)
         {
             _logger = logger;
             _context = context;
+            _userManager = userManager;
         }
 
         public IList<Product> Products { get; set; }
+        public WebUser WebUser { get; set; }
+
 
         public async Task OnGetAsync()
         {
+            if (User.Identity.IsAuthenticated)
+            {
+                WebUser = await _userManager.GetUserAsync(User);
+            }
             var products = from product in _context.Products select product;
             Products = await products.ToListAsync();
         }
--- Startup.cs
+++ Startup.cs
@@ -21,7 +21,10 @@ namespace PagesMariaDB
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages(opt =>
+            {
+                opt.Conventions.AuthorizePage("/Privacy");
+            });
 
             services.AddDbContext<StoreContext>(opt => opt.UseMySql(
                 Configuration.GetConnectionString("StoreContext"),
